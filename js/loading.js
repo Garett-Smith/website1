c30278b3dafc26ca8bcfd5558ec1441fd7f53f7b
Garett-Smith <142698158+Garett-Smith@users.noreply.github.com>
2024-02-27 17:40:09 +0000

    Tue, Feb 27, 2024, 9:40 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -8,6 +8,9 @@ int = setInterval(blurring, 30)
 
 function blurring() {
     load++
+    if (load > 99){
+        clearInterval(int)
+    }
     loadText.innerText = ${load}%
     loadText.style.opacity = 1
     bg.style.filter = "blur(10px)"
