25c0f41480b033f20d93850f2674e708ae03392f
Garett-Smith <142698158+Garett-Smith@users.noreply.github.com>
2024-02-27 17:35:36 +0000

    Tue, Feb 27, 2024, 9:35 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -7,3 +7,10 @@ loadText.innerText = "5%"
 blurring()
 blurring()
 blurring()
+
+function blurring() {
+    load++
+    loadText.innerText = ${load}%
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}
