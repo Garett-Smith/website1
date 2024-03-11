ccb4014f31a92ec2cd78da439de7f2e20482ee30
Garett-Smith <142698158+Garett-Smith@users.noreply.github.com>
2024-03-11 16:51:33 +0000

    Mon, Mar 11, 2024, 9:51 AM -07:00

---
+++ js/landingPage.js
@@ -0,0 +1,12 @@
+toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+model = document.getElementById('model')
+close = document.getElementById('close')
+
+// toggle nav
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+})
+
+//show modal
+
