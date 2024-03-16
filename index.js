d12690d50af59deb997490464ebdfb9ff1911328
Garett-Smith <142698158+Garett-Smith@users.noreply.github.com>
2024-03-15 18:36:03 -0700

    index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById('button1').addEventListener('click', function() {
-    document.getElementById('myText').style.display = 'block';
-});
+function showText() {
+    document.getElementById("myText").style.display = "block";
+}
 
